1731349950dd491714458d358704c215e57d5a65
Yuge <yx692@cornell.edu>
2026-07-17 10:00:12 -0400

    Time: 3 ms (16.34%) | Memory: 19.4 MB (83.95%) - LeetSync

--- 677-map-sum-pairs/map-sum-pairs.py
+++ 677-map-sum-pairs/map-sum-pairs.py
@@ -1,3 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.value = 0
+        self.children = {}
+        self.end_word = False
+
+class MapSum:
+
+    def __init__(self):
+        self.root = TrieNode()
+        self.key_val = {}
+
+    def insert(self, key: str, val: int) -> None:
+        delta = val - self.key_val.get(key,0)
+        node = self.root
+        for char in key:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+                node.children[char].value += val
+            else:
+                node.children[char].value += delta
+            node = node.children[char]
+        node.end_word = True
+        self.key_val[key] = val
+
+    def sum(self, prefix: str) -> int:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return 0
+            node = node.children[char]
+        return node.value
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
+
+
+"""
 class MapSum:
 
     def __init__(self):
@@ -22,3 +64,5 @@ class MapSum:
 # obj = MapSum()
 # obj.insert(key,val)
 # param_2 = obj.sum(prefix)
+
+"""
