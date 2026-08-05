aa87cd08bc3a885958e10d11057bf65ccc4f8a2e
Yuge <yx692@cornell.edu>
2026-08-05 15:42:58 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (12.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,4 +1,60 @@
 from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        visited = set()
+        queue = deque()
+        fresh_count = 0
+        # 先找到所有腐烂橘子
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    visited.add((i,j))
+                    queue.append((i,j))
+                if grid[i][j] == 1:
+                    fresh_count += 1
+        if fresh_count == 0:
+            return 0
+        
+        time = 0
+        while queue and fresh_count > 0:
+            sz = len(queue)
+            for _ in range(sz):
+                node = queue.popleft()
+                directions = [(-1,0),(1,0),(0,-1),(0,1)]
+
+                for dx,dy in directions:
+                    new_x = node[0] + dx
+                    new_y = node[1] + dy
+                    if new_x >= m or new_x < 0 or new_y >= n or new_y < 0:
+                        continue
+                    if (new_x,new_y) not in visited and grid[new_x][new_y] == 1:
+
+                        visited.add((new_x,new_y))
+                        queue.append((new_x,new_y))
+                        fresh_count -= 1
+            time += 1
+        
+        return time if fresh_count == 0 else -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
@@ -41,3 +97,4 @@ class Solution:
                         fresh_count -= 1
             step += 1
         return step if fresh_count == 0 else -1
+"""
