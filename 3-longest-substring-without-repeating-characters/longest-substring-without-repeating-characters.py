fae89c9a0db0547d183dc3779337e5727d3cff62
Yuge <yx692@cornell.edu>
2026-08-06 15:56:49 -0400

    Time: 303 ms (6.77%) | Memory: 19.8 MB (14.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -11,7 +11,7 @@ class Solution:
             while window[c] > 1: # 如果新加入的右边界字符已经在出现在滑动窗口里了
                 d = s[left] # 从最左边界开始一个一个收缩窗口
                 left += 1 
-                window[d] = window.get(d,0) - 1
+                window[d] = window[d] - 1
             # 出了while循环表示当前窗口内的字符已经合法（不含重复字符）
             res = max(res,right-left)
 
