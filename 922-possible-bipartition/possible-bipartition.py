09595b1160ec4a8bc49663c013f29e21571dd223
Yuge <yx692@cornell.edu>
2026-08-10 16:00:01 -0400

    Time: 29 ms (69.00%) | Memory: 24.8 MB (10.53%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -1,10 +1,41 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         graph = [[] for _ in range(n)]
+        for connection in dislikes:
+            node1 = connection[0]-1
+            node2 = connection[1]-1
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+        color = [0] * n
+
+        def dfs(node,c):
+            color[node] = c
+            for neighbor in graph[node]:
+                if color[neighbor] == c:
+                    return False
+                if color[neighbor] == 0:
+                    nxt_color = 3-c
+                    if not dfs(neighbor,nxt_color):
+                        return False
+            return True
+        
+        for i in range(n):
+            if color[i] == 0:
+                if not dfs(i,1):
+                    return False
+        return True
+
+
+
+
+"""
+
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = [[] for _ in range(n)]
         for relation in dislikes:
             node1 = relation[0] - 1
             node2 = relation[1] - 1
-            
             graph[node1].append(node2)
             graph[node2].append(node1)
         
@@ -28,3 +59,5 @@ class Solution:
                     return False
         return True
                 
+
+"""
