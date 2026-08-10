d58269dde60dbe2cc4b7ca420b583f3c5d147926
Yuge <yx692@cornell.edu>
2026-08-10 16:50:25 -0400

    Time: 23 ms (47.78%) | Memory: 19.4 MB (18.20%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        need = {}
+        for c in s1:
+            need[c] = need.get(c,0) + 1
+        
+        valid = 0
+        left,right = 0,0
+        window = {}
+
+        while right < len(s2):
+            c = s2[right]
+            right += 1
+
+            if c in need:
+                window[c] = window.get(c,0) + 1
+                if window[c] == need[c]:
+                    valid += 1
+
+            while right - left >= len(s1):
+                if valid == len(need):
+                    return True
+                
+                d = s2[left]
+                left += 1
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] = window.get(d,0) - 1
+        return False
