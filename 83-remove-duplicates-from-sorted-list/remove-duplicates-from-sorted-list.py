b263591decb4cc5bb26f9445521ecc24e03ce994
Yuge <yx692@cornell.edu>
2026-08-26 11:45:24 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.65%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -6,8 +6,31 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None:
+            return 
+        fast = slow = head
+        while fast:
+            if fast.val != slow.val:
+                slow.next = fast
+                slow = slow.next
+            fast = fast.next
+        slow.next = None
+        return head
+
+
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # slow始终指向"最后一个确认无重复的节点"，fast探索每个节点，遇到和slow不同的值，就把它接到slow后面，slow再前进。循环结束后，断开slow后面可能残留的重复节点。
+        if head is None:
             return
-        
+    
         fast = slow = head
 
         while fast:
@@ -17,3 +40,5 @@ class Solution:
             fast = fast.next
         slow.next = None
         return head
+
+"""
