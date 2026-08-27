9d0de800b0d43257d7738cf261370adbfef023cd
Yuge <yx692@cornell.edu>
2026-08-27 11:35:36 -0400

    Time: 11 ms (31.50%) | Memory: 20.6 MB (21.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,34 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        words = s.split()
+        new_s = ""
+
+        for word in words:
+            for char in word:
+                if char.isalpha() or char.isdigit():
+                    new_s += char.lower()
+                else:
+                    continue
+        print(new_s)
+        left = 0
+        right = len(new_s) - 1
+        while left < right:
+            if new_s[left] != new_s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+
+
+
+
+
+
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         words = s.split(' ')
         processed_s = []
         for word in words:
@@ -18,3 +47,5 @@ class Solution:
                 right -= 1
         
         return True
+
+"""
