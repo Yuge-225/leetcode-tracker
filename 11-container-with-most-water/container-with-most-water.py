6d4a864911caa1ea4d9554bbe0ce4040ad8d0da9
Yuge <yx692@cornell.edu>
2026-08-27 11:51:56 -0400

    Time: 55 ms (73.32%) | Memory: 29.6 MB (38.13%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,5 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        best = 0
+        while left < right:
+            min_height = min(height[left],height[right])
+            curr = (right-left) * min_height
+            best = max(curr,best)
+            # 我们只移动最短柱子，因为每次移动，宽度都减少，如果要找到更大面积，不能移动最长柱子
+            if min_height == height[left]:
+                left += 1
+            else:
+                right -= 1
+        return best
+
+"""
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
         n = len(height)
         left = 0
         right = n - 1
@@ -14,3 +31,7 @@ class Solution:
                 right -= 1
                 
         return max_area
+
+
+
+"""
