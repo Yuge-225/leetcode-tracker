a69d426edae8f71bd10f0bdf1d4dcfd7a2d40435
Yuge <yx692@cornell.edu>
2026-09-22 13:53:06 -0400

    Time: 62 ms (74.98%) | Memory: 19.6 MB (74.41%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,36 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         need = {}
-        for c in t:
-            need[c] = need.get(c,0) + 1
-        left,right = 0,0
-        window ={}
-        valid = 0
-        start = 0
-        length = float("inf")
+        for char in t:
+            need[char] = need.get(char,0) + 1
         
+        window = {}
+        left,right = 0,0
+        valid = 0
+        min_len = float("inf")
+        start = 0
+
         while right < len(s):
             c = s[right]
             right += 1
 
             if c in need:
                 window[c] = window.get(c,0) + 1
-
+                
                 if window[c] == need[c]:
                     valid += 1
-
+            
             while len(need) == valid:
-                if right - left < length:
+                if right - left < min_len:
                     start = left
-                    length = right -left
+                    min_len = right - left
                 
                 d = s[left]
                 left += 1
+
                 if d in need:
                     if window[d] == need[d]:
                         valid -= 1
                     window[d] -= 1
-                
-        return "" if length == float("inf") else s[start:start+length]
-            
+        return "" if min_len == float("inf") else s[start:start+min_len]
 
